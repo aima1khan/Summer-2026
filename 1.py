73adc2291f88a2f8676d34365d2896e838ba42ad
Aimal Khan <aimalkhann727@gmail.com>
2026-06-03 02:20:19 +0500

    Day 1: Hello Python - First commit

---
+++ 1.py
@@ -0,0 +1,11 @@
+name = "Aimal"
+city = "Peshawar"
+university = "GIK Institute"
+cgpa = 3.8
+
+# f-string — like printf in C but much cleaner
+print(f"My name is {name}")
+print(f"I am from {city}")
+print(f"I study at {university}")
+print(f"My CGPA is {cgpa}")
+print(f"In 5 years, I will be {19 + 5} years old")
