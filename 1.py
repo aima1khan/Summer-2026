3cace5b2c0fa68169fe462d9e1768442f9474afb
Aimal Khan <aimalkhann727@gmail.com>
2026-06-03 02:41:37 +0500

    Favourite Number Added

--- 1.py
+++ 1.py
@@ -3,6 +3,7 @@ city = "Peshawar"
 university = "GIK Institute"
 cgpa = 2.6
 adress="AkbarPura"
+fav_no=7
 
 # f-string — like printf in C but much cleaner
 print(f"My name is {name}")
@@ -11,3 +12,4 @@ print(f"I study at {university}")
 print(f"My CGPA is {cgpa}")
 print(f"In 5 years, I will be {19 + 5} years old")
 print(type(adress))
+print(f"Favourite No is{fav_no}")
